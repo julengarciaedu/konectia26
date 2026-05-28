9b4f81406c6b576fd6d9fc3933753efe309cb0d9
julengarciaedu <167200975+julengarciaedu@users.noreply.github.com>
2026-05-28 12:49:18 +0200

    Ejercicios Front (carga inicial)

---
+++ ejerciciosfront/codigo26.js
@@ -0,0 +1,7 @@
+window.addEventListener("load",asignarEventos);
+function asignarEventos(){
+    document.getElementById("boton").addEventListener("click", insertar);
+}
+function insertar(){
+    //código
+}
